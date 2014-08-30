7f959264b32eccd96109ac0a1693d91f30c40300
Luca Zecca <l.zecca78@gmail.com>
2014-08-30 16:43:43 +0200

    fixed error in condition

--- aws/delete_orphans_snapshots.rb
+++ aws/delete_orphans_snapshots.rb
@@ -8,7 +8,7 @@ ec2 = AWS::EC2.new(:region => ec2_region,
    ec2.snapshots.with_owner(:self).select.each  do |snap_object|
    if ec2.volumes["#{snap_object.volume_id}"].exists?
       belong_instance_id = ec2.client.describe_instances(filters: [{ name: "block-device-mapping.volume-id", values: [snap_object.volume_id]}])[:reservation_set][0][:instances_set][0][:instance_id]
-       puts "the snapshot #{snap_object.id} belong to #{ec2.instances[belong_instance_id].tags.each {|key,value| if key == 'Name' and !key.empty? then puts value  else puts belong_instance_id end }}"
+       puts "the snapshot #{snap_object.id} belong to #{ec2.instances[belong_instance_id].tags.each {|key,value| if key == 'Name' and !value.empty? then puts value  else puts belong_instance_id end }}"
    else
        puts "the snapshot #{snap_object.id} doesn't belong to anything right now"
        snapshots_available = ec2.snapshots.filter('volume-id', snap_object.volume_id)
