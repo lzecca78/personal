7d09b9ce2dcfba644f862d78e0cbd1bfcb04407f
Luca Zecca <l.zecca78@gmail.com>
2014-08-30 21:54:17 +0200

    changed behaviour and information of some logic

--- aws/delete_orphans_snapshots.rb
+++ aws/delete_orphans_snapshots.rb
@@ -7,13 +7,13 @@ ec2 = AWS::EC2.new(:region => ec2_region,
   :secret_access_key => AWS_SECRET_KEY )
    ec2.snapshots.with_owner(:self).select.each  do |snap_object|
    if ec2.volumes["#{snap_object.volume_id}"].exists?
-      belong_instance_id = ec2.client.describe_instances(filters: [{ name: "block-device-mapping.volume-id", values: [snap_object.volume_id]}])[:reservation_set][0][:instances_set][0][:instance_id]
-       puts "the snapshot #{snap_object.id} belong to #{ec2.instances[belong_instance_id].tags.each {|key,value| if key == 'Name' and !value.empty? then puts value  else puts belong_instance_id end }}"
+       belong_instance_id = ec2.client.describe_instances(filters: [{ name: "block-device-mapping.volume-id", values: [snap_object.volume_id]}])[:reservation_set][0][:instances_set][0][:instance_id]
+       ec2.instances[belong_instance_id].tags.each {|key,value| if key == 'Name' and !key.empty? then puts "the snapshot #{snap_object.id} belong to #{value}"  elsif key == 'Name' then puts "the snapshot #{snap_object.id} belong to #{belong_instance_id}" end }
    else
-       puts "the snapshot #{snap_object.id} doesn't belong to anything right now"
+       puts "the snapshot #{snap_object.id} belongs to #{snap_object.volume_id} that doesn't exist anymore"
        snapshots_available = ec2.snapshots.filter('volume-id', snap_object.volume_id)
-       snapshots_available.each do |snap_orphan|
-       puts "#{snap_orphan} can be deleted"
+       snapshots_available.each do |snapold|
+       puts "#{snapold.id} can be also deleted because belongs to the same non existent volume_id : #{snap_object.volume_id}"
        end
    end
-  end
+end
